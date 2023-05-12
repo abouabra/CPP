50245ab17a3b89a8c0744154cff27d7237be31a3
Ayman Bouabra <abouabra@c2r3p5.1337.ma>
2023-05-12 18:05:16 +0100

    ex00

--- cpp_03/ex00/ClapTrap.hpp
+++ cpp_03/ex00/ClapTrap.hpp
@@ -2,5 +2,24 @@
 #define CLAPTRAP_HPP
 
 #include <iostream>
+#include <string>
+
+class ClapTrap{
+private:
+    std::string Name;
+    int Hit_Points;
+    int Energy_Points;
+    int Attack_Damage;
+public:
+    ClapTrap();
+    ~ClapTrap();
+    ClapTrap(std::string name);
+    ClapTrap(ClapTrap& claptrap);
+    ClapTrap& operator = (ClapTrap& claptrap);
+
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+};
 
 #endif
--- cpp_03/ex00/main.cpp
+++ cpp_03/ex00/main.cpp
@@ -2,5 +2,17 @@
 
 int main( void ) {
 	
+	ClapTrap john("John Wick");
+	ClapTrap ayman("Ayman");
+	ClapTrap foreign;
+	foreign = ayman;
+
+	john.attack("Ayman");
+	ayman.takeDamage(0);
+	ayman.takeDamage(132);
+	ayman.attack("Me");
+	ayman.beRepaired(2);
+	ayman.takeDamage(10);
+	foreign.attack("John Doe");
 	return 0;
 }
